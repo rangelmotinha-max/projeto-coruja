d9ee588f5bda98720005edd5eeb2f1c1127a400d
rangelmotinha-max <rangelmotinha@gmail.com>
2025-11-17 13:00:20 -0300

    Serve static assets and add SPA fallback

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 
 const env = require('./config/env');
 const routes = require('./src/routes/index.routes');
@@ -10,8 +11,13 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 app.use(errorHandler);
 
