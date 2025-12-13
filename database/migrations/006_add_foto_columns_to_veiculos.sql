f95078ef67a44424109a9cbe71c6883e29a1b805
rangel.mota <rangelmotinha@gmail.com>
2025-12-13 17:49:12 -0300

    remoção de veiculos 2

--- database/migrations/006_add_foto_columns_to_veiculos.sql
+++
@@ -1,9 +0,0 @@
--- Migração 006: adicionar colunas de foto na tabela de veículos
-PRAGMA foreign_keys=OFF;
-
--- Adiciona as colunas necessárias para armazenar metadados de fotos
-ALTER TABLE veiculos ADD COLUMN foto_caminho TEXT;
-ALTER TABLE veiculos ADD COLUMN foto_nome TEXT;
-ALTER TABLE veiculos ADD COLUMN foto_mime TEXT;
-
-PRAGMA foreign_keys=ON;
